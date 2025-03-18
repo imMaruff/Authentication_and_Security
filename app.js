b59c9582fa370f6e4dc22a7ee033d8c21b56e036
Maruf <maruf.khann931@gmail.com>
2025-03-18 13:08:25 +0530

    solved errors

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//jshint esversion:6
 require('dotenv').config()
 const express = require("express");
 const ejs = require("ejs");
@@ -9,6 +8,11 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+ 
+ const findOrCreate = require('mongoose-findorcreate');
+ 
 
 // const encryption = require("mongoose-encryption");
 // const md5 = require("md5");
@@ -35,11 +39,13 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId:String
 })
 
 userSchema.plugin(passportLocalMongoose);
 // userSchema.plugin(encryption, { secret:process.env.SECRET , encryptedFields: ["password"]});
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User",userSchema);
 
@@ -50,10 +56,57 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+}); 
+
+passport.deserializeUser(async function(id, done) {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
+});
+
+
+  passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // userProfileURL: "http://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile.id);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        console.log("User created");
+        console.log(user);
+        
+      return cb(err, user);
+      
+    });
+  }
+));
+
 app.get("/",function(req,res){
     res.render("home");
 });
 
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] })
+);
+  
+
+app.get('/auth/google/secrets', 
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        
+      res.redirect('/secrets');
+    });
+
+
+    
 app.get("/login",function(req,res){
     res.render("login");
 }); 
@@ -61,13 +114,50 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 
-app.get("/secrets",function(req,res){
-    if(req.isAuthenticated()){
-        res.render("secrets")
-    }else{
-        res.render("login")
+app.get("/secrets", async function(req, res) {
+    try {
+      const foundUsers = await User.find({ secret: { $ne: null } });
+      res.render("secrets", { usersWithSecrets: foundUsers });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
     }
-});
+  });
+
+  app.get("/submit", function(req, res){
+    console.log("Session Data:", req.session);
+    console.log("User Authenticated:", req.isAuthenticated());
+    if (req.isAuthenticated()){
+      res.render("submit");
+    } else {
+      res.redirect("/login");
+    }
+  });
+
+  app.post("/submit", async function(req, res) {
+    if (!req.user) {
+      return res.status(401).send("Unauthorized: Please log in.");
+    }
+  
+    if (!req.body.secret || req.body.secret.trim() === "") {
+      return res.status(400).send("Secret cannot be empty.");
+    }
+  
+    try {
+      const foundUser = await User.findById(req.user.id);
+      if (!foundUser) {
+        return res.status(404).send("User not found.");
+      }
+  
+      foundUser.secret = req.body.secret;
+      await foundUser.save();
+      res.redirect("/secrets");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+  
 
 app.get("/logout",function(req,res){
     req.logout(function(err){
